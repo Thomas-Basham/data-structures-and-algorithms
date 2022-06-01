b30541689c46ddffe6ce32b6d649f9b784360bd0
Thomas Basham <Bashamtg@gmail.com>
2022-06-01 16:54:49 -0700

    whiteboard complete

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,69 +1,69 @@
 class Node:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
 
 
 class BinaryTree:
-    """
-                  a
-              b      c
-            d  e    f  g
+	"""
+								a
+						b      c
+					d  e    f  g
 
-      Pre-order: A, B, D, E, C, F
-      In-order: D, B, E, A, F, C
-      Post-order: D, E, B, F, C, A
+		Pre-order: A, B, D, E, C, F
+		In-order: D, B, E, A, F, C
+		Post-order: D, E, B, F, C, A
 
-      Pre-order: root >> left >> right
-      In-order: left >> root >> right
-      Post-order: left >> right >> root
-    """
+		Pre-order: root >> left >> right
+		In-order: left >> root >> right
+		Post-order: left >> right >> root
+	"""
 
-    def __init__(self):
-        self.root = None
-        self.order = []
+	def __init__(self):
+		self.root = None
+		self.order = []
 
-    def pre_order(self):
-      def preorder_traverse(tree, array):
-        if tree is None:
-          return
+	def pre_order(self):
+		def preorder_traverse(tree, array):
+			if tree is None:
+				return
 
-        else:
-          array.append(tree.value)
-          preorder_traverse(tree.left, array)
-          preorder_traverse(tree.right, array)
+			else:
+				array.append(tree.value)
+				preorder_traverse(tree.left, array)
+				preorder_traverse(tree.right, array)
 
-      preorder_traverse(self.root, self.order)
-      return self.order
+		preorder_traverse(self.root, self.order)
+		return self.order
 
-    def in_order(self):
-      def in_order_traverse(tree, array):
-        if tree is None:
-          return
+	def in_order(self):
+		def in_order_traverse(tree, array):
+			if tree is None:
+				return
 
-        else:
-          in_order_traverse(tree.left, array)
-          array.append(tree.value)
-          in_order_traverse(tree.right, array)
+			else:
+				in_order_traverse(tree.left, array)
+				array.append(tree.value)
+				in_order_traverse(tree.right, array)
 
-      in_order_traverse(self.root, self.order)
-      return self.order
+		in_order_traverse(self.root, self.order)
+		return self.order
 
-    def post_order(self):
-      def post_order_traverse(tree, array):
-        if tree is None:
-          return
+	def post_order(self):
+		def post_order_traverse(tree, array):
+			if tree is None:
+				return
 
-        else:
-          post_order_traverse(tree.left, array)
-          post_order_traverse(tree.right, array)
-          array.append(tree.value)
+			else:
+				post_order_traverse(tree.left, array)
+				post_order_traverse(tree.right, array)
+				array.append(tree.value)
 
-      post_order_traverse(self.root, self.order)
-      return self.order
+		post_order_traverse(self.root, self.order)
+		return self.order
 
-    def find_maximum_value(self):
-      self.in_order()
-      self.order.sort()
-      return self.order[-1]
+	def find_maximum_value(self):
+		self.in_order()
+		self.order.sort()
+		return self.order[-1]
