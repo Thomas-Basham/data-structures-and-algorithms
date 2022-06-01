bea6e3018ec60c9d845f918f31e2726fb0843394
Thomas Basham <Bashamtg@gmail.com>
2022-06-01 08:53:53 -0700

    tests passing

--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -1,32 +1,89 @@
 import pytest
 from data_structures.hashtable import Hashtable
 
+"""
+set
+Arguments: key, value
+Returns: nothing
+This method should hash the key, and set the key and value pair in the table, handling collisions as needed.
+Should a given key already exist, replace its value from the value argument given to this method.
+get
+Arguments: key
+Returns: Value associated with that key in the table
+contains
+Arguments: key
+Returns: Boolean, indicating if the key exists in the table already.
+keys
+Returns: Collection of keys
+
+"""
+
 
 def test_exists():
     assert Hashtable
 
 
+def test_create_default():
+    ht = Hashtable()
+    actual = ht.size
+    expected = 1024
+    assert actual == expected
+
+
+def test_hash():
+    """
+    hash
+Arguments: key
+Returns: Index in the collection for that key
+    """
+    ht = Hashtable()
+    index = ht.hash("cat")
+    assert 0 <= index < ht.size
+
+
+def test_set_apple():
+    ht = Hashtable()
+    ht.set("fruit", "apple")
+    fruit_index = ht.hash("fruit")
+    actual = ht.buckets[fruit_index]
+    expected = ("fruit", "apple")
+    assert actual.head.value == expected
+
+
 def test_get_apple():
-    hashtable = Hashtable()
-    hashtable.set("apple", "Used for apple sauce")
-    actual = hashtable.get("apple")
-    expected = "Used for apple sauce"
+    ht = Hashtable()
+    ht.set("fruit", "apple")
+    actual = ht.get("fruit")
+    expected = "apple"
     assert actual == expected
 
 
-def test_internals():
-    hashtable = Hashtable(1024)
-    hashtable.set("ahmad", 30)
-    hashtable.set("silent", True)
-    hashtable.set("listen", "to me")
+def test_collisions():
+    ht = Hashtable()
+    ht.set("cat", "Josie")
+    ht.set("act", "A Contemporary Theater")
+    ht.set("tac", "Taco Tuesday")
 
-    actual = []
+    assert ht.get("cat") == "Josie"
+    assert ht.get("act") == "A Contemporary Theater"
+    assert ht.get("tac") == "Taco Tuesday"
 
-    # NOTE: purposely breaking encapsulation to test the "internals" of Hashmap
-    for item in hashtable.buckets:
-        if item:
-            actual.append(item.display())
 
-    expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
-
+def test_contains():
+    ht = Hashtable()
+    ht.set('fruit', 'apple')
+    expected = True
+    actual = ht.contains('fruit')
     assert actual == expected
+
+
+def test_keys():
+    ht = Hashtable()
+    ht.set('cat','Josie')
+    ht.set('act','A Contemporary Theater')
+    ht.set('tac','Taco Tuesday')
+    actual = ht.keys()
+    expected = {'cat','act','tac'}
+    assert actual == expected
+
+
